1265f56fe83a34e5729565ad31d64aca9633cc9a
Joel-Kaleb <joelkalebmoreno@gmail.com>
2025-10-20 16:42:45 -0600

    Implementa vista para leer excel

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,15 @@
+# import pandas as pd
+# import os
+# from django.db import IntegrityError, transaction
 from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
 
-# Create your views here.
+# --- Funciones Auxiliares ---
+def is_admin(user):
+    return user.is_superuser or user.is_staff
+
+# --- Vista Principal ---
+@user_passes_test(is_admin)
+def importar_proyectos_view(request):
+    # La función solo renderiza la plantilla
+    return render(request, 'RegistroInicial.html')
