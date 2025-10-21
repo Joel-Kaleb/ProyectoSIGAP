504ca03760a8d8bb99824dde1d3ceec6b24f1f6d
Joel-Kaleb <joelkalebmoreno@gmail.com>
2025-10-20 18:12:55 -0600

    Implementa logica para construir direccion en importar_proyecto

--- registration/views.py
+++ registration/views.py
@@ -1,8 +1,15 @@
-# import pandas as pd
-# import os
+import pandas as pd
+import os
+from decouple import config
 # from django.db import IntegrityError, transaction
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+import logging
+from datetime import date
+
+logger = logging.getLogger(__name__)
+
+RUTA_PROCESADOS = config('RUTA_PROCESADOS')
 
 # --- Funciones Auxiliares ---
 def is_admin(user):
@@ -11,5 +18,22 @@ def is_admin(user):
 # --- Vista Principal ---
 @user_passes_test(is_admin)
 def importar_proyectos_view(request):
-    # La función solo renderiza la plantilla
+
+    fecha_actual = date.today()
+    año_actual = fecha_actual.year
+    mes_actual = fecha_actual.month
+
+    calendario_actual = str(año_actual) + ("A" if mes_actual < 7 else "B")
+    
+    NOMBRE_ARCHIVO_BASE = "FORMATO1- (Respuestas).xlsx"
+
+    nombre_archivo_final = NOMBRE_ARCHIVO_BASE.replace('- ', f'-{calendario_actual} ')
+
+    RUTA_COMPLETA = os.path.join(
+        RUTA_PROCESADOS, 
+        str(año_actual) + ('A' if mes_actual < 7 else 'B'), 
+        '1-Procesados', 
+        nombre_archivo_final
+    )
+
     return render(request, 'RegistroInicial.html')
